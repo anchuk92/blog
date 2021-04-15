5ac80ba56751153a8362072b959bf85f278607b5
annab <anchuk92@gmail.com>
2021-04-15 21:22:16 +0300

    init

---
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,45 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Post} from '../../shared/interfaces';
+import {Subscription} from 'rxjs';
+import {PostsService} from '../../shared/posts.service';
+import {AlertService} from '../shared/services/alert.service';
+
+@Component({
+  selector: 'app-dashboard-page',
+  templateUrl: './dashboard-page.component.html',
+  styleUrls: ['./dashboard-page.component.css']
+})
+export class DashboardPageComponent implements OnInit, OnDestroy {
+
+  posts: Post[] = [];
+  pSub: Subscription;
+  dSub: Subscription;
+  searchStr = '';
+
+  constructor(
+    private postsService: PostsService,
+    private alert: AlertService
+  ) { }
+
+  ngOnInit(): void {
+    this.pSub = this.postsService.getAll().subscribe( posts => {
+      this.posts = posts;
+    });
+  }
+  remove(id:string) {
+    this.dSub = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id);
+    });
+    this.alert.danger('Post has been deleted');
+  }
+  ngOnDestroy() {
+    if (this.pSub){
+      this.pSub.unsubscribe();
+    }
+    if (this.dSub){
+      this.dSub.unsubscribe();
+    }
+  }
+}
+
+
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyBcc4SL-GhoPct6G7MxFKUgdLP_oKBqi-k',
+  fbDbUrl: 'https://blog-4a0ce-default-rtdb.firebaseio.com'
+};
